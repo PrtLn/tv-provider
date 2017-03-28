ab140815a6ec591bdc970f08c31c401faa668c68
oleno <pirit.lena@gmail.com>
2017-03-28 23:11:18 +0300

    adding Spring Boot

---
+++ tv-provider/src/main/java/com/example/provider/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.provider.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Page under construction";
+	}
+}
